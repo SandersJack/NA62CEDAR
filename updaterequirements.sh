6f0f79613f2e860db95bdb4cca3389bacd929938
SandersJ <sandersjack141@gmail.com>
2022-11-01 23:20:09 +0100

    updated .sh for requirements

--- updaterequirements.sh
+++ updaterequirements.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 source .venv/Scripts/activate
+git checkout -b update/requirements
 rm requirements.txt 
 pip freeze > requirements.txt
+git add requirements.txt
+git commit -m "Updated requirements.txt"
+git push origin update/requirements
+git branch -d update/requirements
