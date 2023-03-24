58c8cf1edb03d3ff1166c4e96a359c7ba897a2b7
SandersJ <sandersjack141@gmail.com>
2023-03-24 10:32:44 +0000

    Added some fitting

---
+++ ChromFitting.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib import gridspec
+import numpy as np 
+
+fwavelength = [200,225,250,275,300,325,350,375,400]
+fpoints = [0.0575,0.04574,0.04,0.034,0.028,0.025,0.0225,0.0205,0.02] 
+
+
+z = np.polyfit(fwavelength,fpoints,3)
+
+xz = np.linspace(200,400,100)
+
+def eq(z_,x):
+    return z_[0]*x**3 + z_[1]*x**2 + z_[2]*x + z_[3]
+
+def eq4(z_,x):
+    return z_[0]*x**4 + z_[1]*x**3 + z_[2]*x**2 + z_[3]*x + z_[4]
+
+test = eq(z,xz)
+
+print(z)
+
+plt.plot(fwavelength,fpoints ,'.' )
+plt.plot(xz,np.polyval(z, xz))
+plt.plot(xz,test)
+plt.legend(fontsize="small")
+plt.show()
+
---
+++ ManginFitting.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib import gridspec
+import numpy as np 
+
+fwavelength = [200,225,250,275,300,325]
+fwavelength1 = [350,375,400,600] 
+fpoints = [0.8550,0.875,0.8875,0.8925,0.894,0.895]
+fpoints1 = [0.894,0.893,.8925,0.7]
+
+z = np.polyfit(fwavelength,fpoints,3)
+z2 = np.polyfit(fwavelength1,fpoints1,3)
+
+xz = np.linspace(200,335,6)
+xz2 = np.linspace(335,600,50)
+
+def eq(z_,x):
+    return z_[0]*x**3 + z_[1]*x**2 + z_[2]*x + z_[3]
+
+def eq4(z_,x):
+    return z_[0]*x**4 + z_[1]*x**3 + z_[2]*x**2 + z_[3]*x + z_[4]
+
+test = eq(z,xz)
+test2 = eq(z2,xz2)
+
+print(z)
+print(z2)
+
+plt.plot(fwavelength,fpoints ,'.' )
+plt.plot(xz,np.polyval(z, xz))
+plt.plot(xz,test)
+plt.plot(fwavelength1,fpoints1 ,'.' )
+plt.plot(xz2,np.polyval(z2, xz2))
+plt.plot(xz2,test2)
+plt.legend(fontsize="small")
+plt.show()
+
